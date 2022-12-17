f717800a108d01d04b03f012bcc69b61a0c7f95b
AF <100952220+ibnbayo@users.noreply.github.com>
2022-12-17 23:42:04 +0100

    Add length to text

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi, createGeneration } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -7,22 +7,23 @@ const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
   const completion = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
+    model: "text-davinci-003",
+    prompt: generatePrompt(req.body.word),
+    temperature: 1,
+    max_tokens: 1000,
   });
+
+  console.log(`Generate js 17 is ${completion.data}`)
+  // stream.on('data', (chunk) => {
+  //   // Handle the data chunk here
+  //   console.log(chunk.toString());
+  // });
+
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(animal) {
+function generatePrompt(word) {
   const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+    word[0].toUpperCase() + word.slice(1).toLowerCase();
+  return `Write a 100 word article about  ${capitalizedAnimal}`;
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [wordInput, setWordInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -13,32 +13,48 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({ word: wordInput }),
     });
     const data = await response.json();
+    console.log(`Index js 19 is ${data}`)
     setResult(data.result);
-    setAnimalInput("");
+    setWordInput("");
   }
+  //The above code is defining an async function called onSubmit, 
+  //which is meant to handle a form submission event. When the form is submitted, 
+  //the function makes a POST request to the /api/generate endpoint, 
+  //with the animalInput variable as the request body.
+
+//The headers property specifies the request headers, 
+//in this case setting the Content-Type to application/json. 
+//The body property specifies the request body, in this case a JSON string 
+//representation of an object with a single property called animal, 
+//whose value is the current value of the animalInput variable.
+
+//The function then waits for the response from the server 
+//and parses the JSON data contained in the response.
+// The result property of the data is then set as the value of the result 
+//state variable, and the animalInput state variable is reset to an empty string.
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
+        <title>OpenAI Fiddle</title>
         <link rel="icon" href="/dog.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        
+        <h3>Article generator</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="word"
+            placeholder="Enter a word or phrase"
+            value={wordInput}
+            onChange={(e) => setWordInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate article" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
